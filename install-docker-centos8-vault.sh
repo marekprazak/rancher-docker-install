54a67d2cec3ea66e93a27a3589981ef2dfd67632
marekprazak <138453407+marekprazak@users.noreply.github.com>
2026-05-21 18:51:42 +0200

    Update Docker installation script for CentOS 8

--- install-docker-centos8-vault.sh
+++ install-docker-centos8-vault.sh
@@ -7,26 +7,31 @@ sudo rm -f /etc/yum.repos.d/*.repo
 sudo tee /etc/yum.repos.d/centos-vault.repo >/dev/null <<'EOF'
 [baseos]
 name=CentOS-8.5.2111 BaseOS
-baseurl=http://vault.centos.org/8.5.2111/BaseOS/x86_64/os/
+baseurl=https://archive.kernel.org/centos-vault/8.5.2111/BaseOS/x86_64/os/
 enabled=1
 gpgcheck=0
 
 [appstream]
 name=CentOS-8.5.2111 AppStream
-baseurl=http://vault.centos.org/8.5.2111/AppStream/x86_64/os/
+baseurl=https://archive.kernel.org/centos-vault/8.5.2111/AppStream/x86_64/os/
 enabled=1
 gpgcheck=0
 
 [extras]
 name=CentOS-8.5.2111 Extras
-baseurl=http://vault.centos.org/8.5.2111/extras/x86_64/os/
+baseurl=https://archive.kernel.org/centos-vault/8.5.2111/extras/x86_64/os/
 enabled=1
 gpgcheck=0
 EOF
 
 sudo dnf clean all
+
 sudo dnf install -y yum-utils device-mapper-persistent-data lvm2
+
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
 sudo dnf install -y docker-ce-20.10.24 docker-ce-cli-20.10.24 containerd.io --setopt=install_weak_deps=False
+
 sudo systemctl enable --now docker
+
 docker --version
