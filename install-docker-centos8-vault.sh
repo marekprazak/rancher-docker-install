2dbb0b2e3a04a63cdf435ea6237925e0b6d333d4
marekprazak <138453407+marekprazak@users.noreply.github.com>
2026-05-21 15:27:07 +0200

    Update install-docker-centos8-vault.sh

--- install-docker-centos8-vault.sh
+++ install-docker-centos8-vault.sh
@@ -1,10 +1,9 @@
-sudo tee /tmp/install-wrapper.sh > /dev/null <<'EOF'
 #!/bin/sh
 set -eux
 
 rm -f /etc/yum.repos.d/*.repo
 
-cat >/etc/yum.repos.d/centos-vault.repo <<'EOR'
+cat >/etc/yum.repos.d/centos-vault.repo <<'EOF'
 [baseos]
 name=CentOS-8.5.2111 BaseOS
 baseurl=http://vault.centos.org/8.5.2111/BaseOS/x86_64/os/
@@ -22,17 +21,11 @@ name=CentOS-8.5.2111 Extras
 baseurl=http://vault.centos.org/8.5.2111/extras/x86_64/os/
 enabled=1
 gpgcheck=0
-EOR
+EOF
 
 dnf clean all
-
 dnf install -y yum-utils device-mapper-persistent-data lvm2
-
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
 dnf install -y docker-ce-20.10.24 docker-ce-cli-20.10.24 containerd.io --setopt=install_weak_deps=False
-
 systemctl enable --now docker
-
 docker --version
-EOF
